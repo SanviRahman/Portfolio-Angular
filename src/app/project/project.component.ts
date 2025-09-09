75f69e044f4467323d33542669bd55490f5cdc95
Sheikh Forid Ahmed Shanto <Sheikh15-3700@diu.edu.bd>
2025-09-09 12:03:01 +0600

    Update Done

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,14 +1,32 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
-export class ProjectComponent {
+export class ProjectComponent implements AfterViewInit, OnDestroy {
   @ViewChild('projectsContainer') projectsContainer!: ElementRef;
+  private autoScrollInterval: any;
 
-  scrollProjects(distance: number) {
-    this.projectsContainer.nativeElement.scrollBy({ left: distance, behavior: 'smooth' });
+  ngAfterViewInit() {
+    this.autoScrollInterval = setInterval(() => {
+      if (this.projectsContainer) {
+        const container = this.projectsContainer.nativeElement;
+        const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+        if (container.scrollLeft >= maxScrollLeft) {
+          container.scrollTo({ left: 0, behavior: 'smooth' }); // আবার শুরু থেকে শুরু করবে
+        } else {
+          container.scrollBy({ left: 300, behavior: 'smooth' });
+        }
+      }
+    }, 3000); // প্রতি ৩ সেকেন্ডে অটো স্ক্রল
+  }
+
+  ngOnDestroy() {
+    if (this.autoScrollInterval) {
+      clearInterval(this.autoScrollInterval);
+    }
   }
 }
